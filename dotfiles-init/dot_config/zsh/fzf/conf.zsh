b157ceb500b8d4dbccf7d0a7da80fb024a5bca86
Nicole Coard <coardnicole@gmail.com>
2024-05-23 11:51:42 -0400

    remove duplicated init directory

--- dotfiles-init/dot_config/zsh/fzf/conf.zsh
+++
@@ -1,6 +0,0 @@
-# vim: ft=zsh :
-
-(( $+commands[fzf] )) || return
-
-source $XDG_DATA_HOME/zsh/plugins/fzf/shell/completion.zsh
-source $XDG_DATA_HOME/zsh/plugins/fzf/shell/key-bindings.zsh
--- dotfiles-init/dot_config/zsh/rancher/path.zsh
+++
@@ -1,4 +0,0 @@
-RD_BIN="$HOME/.rd/bin"
-if [[ -d "$RD_BIN" ]]; then
-  path=("$RD_BIN" $path)
-fi
